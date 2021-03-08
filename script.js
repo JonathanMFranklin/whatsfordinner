d6bf2b8eb7aa65190f833092f87e304ffe2cf9eb
yonimacaronii <yonimacaronii@users.noreply.github.com>
2021-03-08 13:14:57 -0500

    added array in js

---
+++ script.js
@@ -0,0 +1,21 @@
+// Array to hold restaurants 
+const restaurants = [
+    'Sarang',
+    'Aji Sushi and Asian Cuisine',
+    'Monnik',
+    'New Wave Burritos',
+    'Four Pegs',
+    'Dakshin',
+    'The Post',
+    'Hammerheads',
+    'Pizza Donisi',
+    'Simply Thai',
+    'Queen of Sheba',
+    'Dragon King\'s Daughter',//the slash is an escape character to allow 
+    'Mi Sueño',
+    'Royals',
+    'Back Deck BBQ',
+    'El Taco Luchador',
+
+
+]
