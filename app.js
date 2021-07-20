622ac0d5d860d61e0174575cfbb5a42e3c39085d
yonimacaronii <yonimacaronii@users.noreply.github.com>
2021-07-20 16:57:56 -0400

    added code to get form data

--- app.js
+++ app.js
@@ -15,3 +15,41 @@ email.addEventListener('input', function(e){
   }
  
 });
+
+ // get the form element from dom
+ const formElement = document.getElementById('tellMe')
+
+ // convert the form to JSON
+ const getFormJSON = (form) => {
+   const data = new FormData(form);
+   return Array.from(data.keys()).reduce((result, key) => {
+     result[key] = data.get(key);
+     return result;
+   }, {});
+ };
+
+ // handle the form submission event, prevent default form behaviour, check validity, convert form to JSON
+ const handler = (event) => {
+   event.preventDefault();
+   const email = document.getElementById("email").value;
+   const recommendation = document.getElementById("recommendation").value;
+   const name = document.getElementById("name").value;
+
+   const valid = formElement.reportValidity();
+   if (valid) {
+     fetch("http://127.0.0.1:8000/post-form", {
+     method: "POST",
+     headers: {
+       "Content-Type": "application/json",
+       Accept: "application/json",
+     },
+     body: JSON.stringify({
+       name: name,
+       email: email,
+       recommendation: recommendation,
+     }),
+   });
+ }
+}
+ 
+ formElement.addEventListener("submit", handler);
