2ee68c8375405a87936de1bde13730023c9b4cfb
yonimacaronii <yonimacaronii@users.noreply.github.com>
2021-07-20 16:52:25 -0400

    file for server to run

---
+++ server.js
@@ -0,0 +1,71 @@
+var http = require("http");
+var fs = require("fs");
+var path = require("path");
+
+http
+  .createServer(function (request, response) {
+    console.log("Request: ", request.url);
+    var filePath = "./" + request.url;
+    if (request.url === "/") {
+      filePath = "./index.html";
+    }
+    if (request.url === "/post-form") {
+      request.on("data", (jsonData) => {
+        const data = JSON.parse(jsonData.toString());
+        console.log(data);
+        // Save the data to a file.
+        jsonData = jsonData.toString() + "\n";
+        fs.writeFile('submission.txt', jsonData, {flag: 'a' }, err => {
+            if (err) {
+              console.error(err)
+              return
+            }
+            //file written successfully
+          })
+
+        response.writeHead(200);
+        response.end();
+      });
+    }
+    var extname = String(path.extname(filePath)).toLowerCase();
+    var mimeTypes = {
+      ".html": "text/html",
+      ".js": "text/javascript",
+      ".css": "text/css",
+      ".json": "application/json",
+      ".png": "image/png",
+      ".jpg": "image/jpg",
+      ".gif": "image/gif",
+      ".svg": "image/svg+xml",
+      ".wav": "audio/wav",
+      ".mp4": "video/mp4",
+      ".woff": "application/font-woff",
+      ".ttf": "application/font-ttf",
+      ".eot": "application/vnd.ms-fontobject",
+      ".otf": "application/font-otf",
+      ".wasm": "application/wasm",
+    };
+    var contentType = mimeTypes[extname] || "application/octet-stream";
+    fs.readFile(filePath, function (error, content) {
+      if (error) {
+        if (error.code == "ENOENT") {
+          fs.readFile("./404.html", function (error, content) {
+            response.writeHead(404, { "Content-Type": "text/html" });
+            response.end(content, "utf-8");
+          });
+        } else {
+          response.writeHead(500);
+          response.end(
+            "Sorry, check with the site admin for error: " +
+              error.code +
+              " ..\n"
+          );
+        }
+      } else {
+        response.writeHead(200, { "Content-Type": contentType });
+        response.end(content, "utf-8");
+      }
+    });
+  })
+  .listen(8000);
+console.log("Server running at http://127.0.0.1:8000/");
